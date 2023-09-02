2874c4a81acbe7b7fabc421f0dba7cdda6d946aa
Alex Wong <wonghoming111105@gmail.com>
2023-09-01 23:07:49 -0400

    update

---
+++ markdown-previewer/src/components/HeaderContainer.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Header(onToggleGuide: any) {
+  let StyleHeader = styled.h1``;
+
+  return (
+    <>
+      <StyleHeader onClick={() => onToggleGuide} />
+    </>
+  );
+}
+
+export default function HeaderContainer() {
+  return <Header />;
+}
