5f4a6815a9c8f11630b858f2ce4437f795db442f
Alex Wong <wonghoming111105@gmail.com>
2023-09-15 18:12:46 -0400

    update profile

--- my-profile/app/page.tsx
+++ my-profile/app/page.tsx
@@ -1,8 +1,8 @@
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
-import profileIcon from "../public/1559I.jpg"
-import nextJspPic from "../public/nextjs.png"
-import tailwindpPic from "../public/tailwind.png"
+import profileIcon from "../public/1559I.jpg";
+import nextJspPic from "../public/nextjs.png";
+import tailwindpPic from "../public/tailwind.png";
 
 export default function Home() {
   return (
@@ -10,38 +10,47 @@ export default function Home() {
       <main className="flex flex-col h-screen">
         <NavBar />
         <div className="flex flex-row h-full items-center justify-center">
-          <div className="flex w-1/2 flex-col h-screen ml-80 justify-center">
-            <h1 className="text-left text-5xl">
-              Hello There!👋
-            </h1>
+          <div className="flex w-1/2 flex-col h-screen pl-24 justify-center">
+            <h1 className="text-left text-5xl">Hello There!👋</h1>
             <br />
-            <h2>
-              Welcome!
-            </h2>
+            <h1 className="text-left text-3xl">Welcome!</h1>
             <br />
             <p>
-              I am a seasoned Development Engineer with 3+ years of software engineering, mobile development, backend technology, and technical operations experience. Recognized for demonstrating a natural aptitude for applying research and development strategies, as well as for establishing successful software engineering. Professional focal points include technical proficiencies, data analysis, communication skills, industry expertise, and the ability to adapt to ever-changing situations in a fast-paced environment. Delivering superior administration in these areas expertise requires utilization of effective interpersonal skills, troubleshooting, and a keen eye for detail, as well as independent problem-solving, time management, programming languages, process optimization, project coordination, and resource allocation expertise to support overall efficiency and deliver optimal results.
+              I am a seasoned Development Engineer with 3+ years of software
+              engineering, mobile development, backend technology experience.
+              Recognized for demonstrating a natural aptitude for applying
+              research and development strategies, as well as for establishing
+              successful software engineering.
             </p>
             <br />
-            <p>
-              This web build with:
-            </p>
+            <p>This web build with:</p>
             <br />
-            <div className="flex flex-row ">
-              <Image src={nextJspPic} alt="nextjs" className="p-5"></Image>
-              <Image src={tailwindpPic} alt="tailwind" className="ml-15 p-10"></Image>
+            <div className="flex flex-row h-40">
+              <Image
+                src={nextJspPic}
+                alt="nextjs"
+                className="mr-10"
+                width={160}
+              ></Image>
+              <Image
+                src={tailwindpPic}
+                alt="tailwind"
+                className="ml-30"
+                width={150}
+              ></Image>
             </div>
-
           </div>
           <div className="flex flex-col w-1/2 justify-items-center items-center	">
             <div className="w-6/12">
               <div className="p-10">
-                <img src={profileIcon.src} alt="..." className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                <img
+                  src={profileIcon.src}
+                  alt="..."
+                  className="shadow rounded-full max-w-full h-auto align-middle border-none"
+                />
               </div>
             </div>
-            <p className="text-center">
-              Alex Wong
-            </p>
+            <p className="text-center">Alex Wong</p>
           </div>
         </div>
       </main>
--- my-profile/components/NavBar.tsx
+++ my-profile/components/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
     <>
       <nav className="flex items-center justify-between flex-wrap bg-black p-6 sticky top-0">
         <div className="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
+          {/* <svg
             className="fill-current h-8 w-8 mr-2"
             width="54"
             height="54"
@@ -13,7 +13,7 @@ export default function NavBar() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
-          </svg>
+          </svg> */}
           <span className="font-semibold text-xl tracking-tight">
             Homing.dev
           </span>
